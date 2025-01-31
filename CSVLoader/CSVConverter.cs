371d19cea85e2811c86baf331f9b5cfa7e3599a5
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2025-01-31 08:51:58 +0100

    separator dynamic

--- CSVLoader/CSVConverter.cs
+++ CSVLoader/CSVConverter.cs
@@ -4,42 +4,57 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CSVLoader
 {
    internal static class CSVConverter
     {
-        public static string[]OutputCSV(ObservableCollection<Person> persons)
+        public static string[]OutputCSV(ObservableCollection<Person> persons,string separator)
         {
+            
+            
             string[] tempArr = new string[persons.Count+1]; //+1 for header
-            tempArr[0] = "Vorname;Nachname;Email;GebDat;ID";
+            tempArr[0] = $"Vorname{separator}Nachname{separator}Email{separator}GebDat{separator}ID";
             for (int count = 0; count < persons.Count; count++)
             {
-                tempArr[count + 1] = $"{persons[count].Vorname};{persons[count].Nachname};{persons[count].Email};{persons[count].GebDat};{persons[count].ID}";
+                tempArr[count + 1] = $"{persons[count].Vorname}{separator}" +
+                             $"{persons[count].Nachname}{separator}" +
+                             $"{persons[count].Email}{separator}" +
+                             $"{persons[count].GebDat}{separator}" +
+                             $"{persons[count].ID}";
 
             }
 
             return tempArr;
         }
-        public static ObservableCollection<Person> ImportCSV(string[] importText)
+        public static ObservableCollection<Person> ImportCSV(string[] importText,string separator)
         {
+            
             ObservableCollection<Person> tempList = new ObservableCollection<Person>();
             if (importText.Length > 1)
             {
                 for (int count = 1; count< importText.Length; count++)
                 {
-                    string[] tempString = importText[count].Split(';');
+                    //out of bounds error only if separator isnt chosen at load because substring doesnt exist 
                     Person tempPerson = new Person();
-                    tempPerson.Vorname = tempString[0];
-                    tempPerson.Nachname = tempString[1];
-                    tempPerson.Email = tempString[2];
-                    tempPerson.GebDat = DateTime.Parse(tempString[3]);
-                    int.TryParse(tempString[4], out int result);
-                    tempPerson.ID = result;
-                    tempList.Add(tempPerson);
+                    string[] tempString = importText[count].Split(separator);
+                    if (tempString.Length != 5)
+                    {
+                        throw new FormatException("CSV im falschen Format oder Leer");
+                    }
 
-
+                        tempPerson.Vorname = tempString[0];
+                        tempPerson.Nachname = tempString[1];
+                        tempPerson.Email = tempString[2];
+                        tempPerson.GebDat = DateTime.Parse(tempString[3]);
+                        int.TryParse(tempString[4], out int result);
+                        tempPerson.ID = result;
+                        tempList.Add(tempPerson);
+                    
                 }
+                
             }
             return tempList;
         }
--- CSVLoader/MainWindow.xaml.cs
+++ CSVLoader/MainWindow.xaml.cs
@@ -20,8 +20,7 @@ namespace CSVLoader
     public partial class MainWindow : Window
     {
         public ObservableCollection<Person> People { get; set; }
-
-     
+        public string CurrentTrenn { get; set; }
         public MainWindow()
         {
             People = new ObservableCollection<Person>();
@@ -37,6 +36,7 @@ namespace CSVLoader
             person.Vorname = tbx_vorname.Text;
             person.Nachname = tbx_nachname.Text;
             person.GebDat = dtp_gebdat.SelectedDate ==null ? new DateTime():(DateTime)dtp_gebdat.SelectedDate;
+            
             int temp = 0;
             int.TryParse(tbx_id.Text,out temp);
             person.ID = temp;
@@ -48,12 +48,13 @@ namespace CSVLoader
         private void btn_save_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            
             saveFileDialog.DefaultExt = ".csv";
             saveFileDialog.Filter = "CSV file (*.csv)|*.csv|Text file (*.txt) | *.txt | All files (*.*)|*.* ";
             saveFileDialog.AddExtension = true;
             if(saveFileDialog.ShowDialog()==true)
             { 
-                File.WriteAllLines(saveFileDialog.FileName,CSVConverter.OutputCSV(People));
+                File.WriteAllLines(saveFileDialog.FileName,CSVConverter.OutputCSV(People,CurrentTrenn));
             }
         }
 
@@ -63,11 +64,54 @@ namespace CSVLoader
             openFileDialog.Filter= "CSV file (*.csv)|*.csv|Text file (*.txt) | *.txt | All files (*.*)|*.* ";
             if (openFileDialog.ShowDialog() == true)
             {
-                foreach (var item in CSVConverter.ImportCSV(File.ReadAllLines(openFileDialog.FileName)) )
+                try
                 {
-                    People.Add(item);
+                    foreach (var item in CSVConverter.ImportCSV(File.ReadAllLines(openFileDialog.FileName), CurrentTrenn))
+                    {
+
+                        People.Add(item);
+                    }
                 }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
             }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+           
+            
+            if (btn_radioSem.IsChecked == true)
+            {
+                CurrentTrenn = ";";
+            }
+            else if (btn_radioComm.IsChecked == true)
+            {
+                CurrentTrenn = ",";
+            }
+            else if(btn_radioUnder.IsChecked == true)
+            {
+                CurrentTrenn = "_" ;
+            }
+            else if (btn_radioOther.IsChecked == true)
+            {
+                CurrentTrenn = tb_inputOther.Text;
+
+            }
+          
+
+
+          
+        }
+
+        private void tb_inputOther_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btn_radioOther.IsChecked = true;
+            CurrentTrenn = tb_inputOther.Text;
         }
     }
 }
